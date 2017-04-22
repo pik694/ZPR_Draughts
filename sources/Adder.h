709eee67c269357b1c9d7b5c8f7d813ccf8efed0
piotr <piotr.zelazko@gmail.com>
2017-04-22 14:54:44 +0200

    Checking Boost Tests

---
+++ sources/Adder.h
@@ -0,0 +1,17 @@
+//
+// Created by Piotr Żelazko on 22.04.2017.
+//
+
+#ifndef ZPR_DRAUGHTS_ADDER_H
+#define ZPR_DRAUGHTS_ADDER_H
+
+
+class Adder {
+public:
+    int add (int _lh, int _rh){
+        return _lh + _rh;
+    }
+};
+
+
+#endif //ZPR_DRAUGHTS_ADDER_H
---
+++ sources/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Adder.h"
+
+int main() {
+
+    Adder adder;
+    std::cout << adder.add(1,3) << std::endl;
+    return 0;
+}
