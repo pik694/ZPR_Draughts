f483501a5314972db2867c306c9171d5a03b34e3
mwilczy <mwilczy2@mion.elka.pw.edu.pl>
2017-05-30 02:20:01 +0200

    new functionalities to web client

--- web_client/gui.js
+++ web_client/gui.js
@@ -1,40 +1,77 @@
 
 var mySocket = null
-
+var currentRoom = ""
+var currentNick = ""
 window.onload = function() {
+    generateRooms();
 	mySocket = new WebSocket("ws://127.0.0.1:9002");
 	mySocket.onmessage = function(event) {
 		var msg = JSON.parse(event.data);
 		console.log(event.data);
 	}
-	$("button").on("click",function(event) {
-		hideAll();
-		$("#room_form").show("slow");
-		console.log("button clicked");
-		var msg = {
-        	type: "nickRequest",
-        	value: document.getElementById("nickname").value
-        };
-        if(mySocket.readyState === WebSocket.OPEN)
-        	mySocket.send(JSON.stringify(msg));
-	});
+	$("button").on("click",nickButton);
 	InitGame();
 }
 
+// game form
 
-function hideAll() {
-	$("#room_form").hide();
-	$("#game_form").hide();
-	$("#nick_form").hide();
+function gameSwitch() {
+    hideAll();
+    document.getElementById("nick_output").innerHTML = currentNick;
+    document.getElementById("room_name_output").innerHTML = currentRoom;
+    $("#game_form").show("slow");
 }
 
+
+
+
+// room form
 
 function generateRooms() {
-	// code 
-
+    var elements = document.getElementsByClassName("room");
+    console.log("aww");
+    for(var i=0;i<elements.length;i++) {
+        console.log("aww");
+        elements[i].addEventListener("click",roomClickHandler,false);
+    }
+}
+function roomClickHandler() {
+    currentRoom = this.innerHTML;
+    gameSwitch();
 }
 
 
+
+
+
+// utility functions
+function hideAll() {
+    $("#room_form").hide();
+    $("#game_form").hide();
+    $("#nick_form").hide();
+}
+
+
+
+// nick form
+function nickButton(event) {
+    currentNick = document.getElementById("nickname").value;
+    hideAll();
+    $("#room_form").show("slow");
+    console.log("button clicked");
+    var msg = {
+        type: "nickRequest",
+        value: document.getElementById("nickname").value
+    };
+    if(mySocket.readyState === WebSocket.OPEN)
+        mySocket.send(JSON.stringify(msg));
+}
+
+
+
+
+
+// network communication
 function communication() {
     var mySocket = new WebSocket("ws://127.0.0.1:9002");
     mySocket.onerror = function(evt)
