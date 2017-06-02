9386ce97c611059f40b42515becc14342131e566
mwilczy <mwilczy2@mion.elka.pw.edu.pl>
2017-06-02 04:51:59 +0200

    all signals implemented

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/john/checkers
-# Build directory: /home/john/checkers/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sources")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/john/checkers/tests
-# Build directory: /home/john/checkers/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
