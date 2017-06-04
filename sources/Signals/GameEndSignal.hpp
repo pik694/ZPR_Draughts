35b042d7df4481ee68ad79f92bd3a5b47195df26
Piotr Żelazko <pik9603@gmail.com>
2017-06-04 20:25:08 +0200

    Written a part of the documentation.

--- sources/Signals/GameEndSignal.hpp
+++ sources/Signals/GameEndSignal.hpp
@@ -5,28 +5,65 @@
 #ifndef ZPR_DRAUGHTS_GAMEENDSIGNAL_HPP
 #define ZPR_DRAUGHTS_GAMEENDSIGNAL_HPP
 
+/*!
+ * @file
+ * @brief Class representing a signal sent from server to the client, when a game ends.
+ * Derives from Signal class
+ */
 
 #include "Signal.hpp"
 #include "SignalFactory.hpp"
 
+/*!
+ * @brief Class representing a signal sent from server to the client, when a game ends.
+ * Derives from Signal class
+ */
 class GameEndSignal : public Signal {
 public:
+
+    /*!
+     * @brief Constructor used by SignalFactory
+     */
     GameEndSignal() : won_(false) {}
 
+    /*!
+     * @brief Default constructor.
+     * @param hdl sender
+     * @param won whether receiver won or lost
+     */
     GameEndSignal(ConnectionProtocolHandler *hdl, bool won) : Signal(hdl), won_(won) {}
 
+    /*!
+     * @brief Changes an object into Json::Value
+     * @return Json::Value of this object
+     */
     virtual Json::Value serialize();
-    
+
+    /*!
+     *
+     * @return true if receiver won
+     */
     const bool getWon() const {
         return won_;
     }
 
+    /*!
+     * @brief Reimplemented virtual method from Signal
+     * Implemented according to the instructions given in @link Signal::acceptDispatcher @endlink documentation.
+     */
     virtual void acceptDispatcher(MessageDispatcher &);
 
-    static Register<GameEndSignal> reg;
+
 private:
+
+    /*!
+     * @static
+     * @brief Used to register this type in SignalFactory
+     */
+    static Register<GameEndSignal> reg;
+
     const bool won_;
-    
+
 };
 
 
--- sources/Signals/LeaveRoomSignal.hpp
+++ sources/Signals/LeaveRoomSignal.hpp
@@ -5,21 +5,51 @@
 #ifndef ZPR_DRAUGHTS_LEAVEROOMSIGNAL_HPP
 #define ZPR_DRAUGHTS_LEAVEROOMSIGNAL_HPP
 
+/*!
+ * @file
+ * @brief Class representing a leave room request.
+ * Derives from Signal class
+ */
 
 #include "Signal.hpp"
 #include "SignalFactory.hpp"
 
+
+/*!
+ * @brief Class representing a leave room request.
+ * Derives from Signal class
+ */
 class LeaveRoomSignal : public Signal {
 
 public:
+    /*!
+     * @brief Constructor used by SignalFactory
+     */
     LeaveRoomSignal() {}
 
+    /*!
+     * @brief Default constructor.
+     * @param hdl sender
+     */
     LeaveRoomSignal(ConnectionProtocolHandler *hdl) : Signal(hdl) {}
 
+    /*!
+     * @brief Reimplemented virtual method from Signal
+     * Implemented according to the instructions given in @link Signal::acceptDispatcher @endlink documentation.
+     */
     virtual void acceptDispatcher(MessageDispatcher &);
 
+    /*!
+     * @brief Changes an object into Json::Value
+     * @return Json::Value of this object
+     */
     virtual Json::Value serialize();
+private:
 
+    /*!
+     * @static
+     * @brief Used to register this type in SignalFactory
+     */
     static Register<LeaveRoomSignal> reg;
 };
 
