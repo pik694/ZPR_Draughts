6eb09a9a101cdcfc85cb4d0bd862c74952f45322
mwilczy <mwilczy2@mion.elka.pw.edu.pl>
2017-06-06 21:52:38 +0200

    windows build fixes

---
+++ sources/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/John/Desktop/checkers/sources
+# Build directory: C:/Users/John/Desktop/checkers/sources
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/John/Desktop/checkers/tests
+# Build directory: C:/Users/John/Desktop/checkers/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(boostUnitTestsRun "Tests")
